9dec04bdc860f73af9246b4b34427e2a7ede4db6
soniya-mi <39825519+soniya-mi@users.noreply.github.com>
2020-01-02 16:45:51 +0530

    Create print Nth word in a given string.py
    The original string is : GFG is for Geeks
    The Nth word in String : for

---
+++ interviewbit/print Nth word in a given string.py
@@ -0,0 +1,12 @@
+# initializing string
+test_str = "GFG is for Geeks"
+
+# printing original string
+print("The original string is : " + test_str)
+
+# initializing N
+N = 3
+
+
+res =test_str.split(' ')[N-1]
+print res
